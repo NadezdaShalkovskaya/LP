7ff43310c265f5c36389b1d629e6d4d4afba93be
Надежда <@DESKTOP-EHSPKBR>
2024-12-21 20:12:33 +0200

    Добавьте файлы проекта.

---
+++ LP/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите количество учащихся в классе:");
+        int n =int.Parse(Console.ReadLine());
+        double[] heights = new double[n];
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Введите рост учащегося {i + 1}(в см)");
+            heights[i]=double.Parse(Console.ReadLine());
+        }
+        double averageHeight = CalculateAverage (heights);
+        Console.WriteLine($"Средний рост учащегося в классе:{averageHeight} см ");
+    }
+    static double CalculateAverage (double[] heights)
+    {
+        double sum = 0;
+        for (int i = 0;i < heights.Length;i++)
+        {
+            sum += heights[i];
+        }
+        return sum/heights.Length;
+    }
+}
